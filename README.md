8c25435b6fbff684f60ba45ef0c71f087cc1c943
Ryan Hodge <ryanhodge240@gmail.com>
2023-01-17 17:59:38 -0500

    Setup the autonomous parameters

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # JPL Autonomous Code and Setup
+<b> If there are problems with depth, recalibrate zed stereo camera </b>
 
-Testing commit
+## Setup
+```sh
+roslaunch zed_wrapper zedm.launch
+rosrun dynamic_reconfigure dynparam set /zedm/zed_node depth_confidence 99
+rosrun dynamic_reconfigure dynparam set /zedm/zed_node depth_texture_conf 90
+rosrun dynamic_reconfigure dynparam set /zedm/zed_node depth_confidence 100
+roslaunch zed_rtabmap_example zed_rtabmap.launch camera_model:=zedm
+```
--- test_subscriber/scripts/depth_sensing.py
+++ test_subscriber/scripts/depth_sensing.py
@@ -1,34 +1,52 @@
 #!/usr/bin/env python
-import math
 import rospy
-import cv2
+import ros_numpy
+import numpy as np
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2
 
 def depth_callback(msg):
     """Take in an image, convert it to a computer vision image, 
     then get the value of the center pixel which should be 
     the distance from the center of the camera."""
     # type: (Image) -> None
-    
-    try:
-        cv_image = CvBridge().imgmsg_to_cv2(msg, "passthrough")
-    except CvBridgeError as e:
-        print(e)
+
+    depth = ros_numpy.image.image_to_numpy(msg)
 
     # Get the center of the image matrix
-    u = len(cv_image) / 2
-    v = len(cv_image[0]) / 2
+    u = len(depth) / 2
+    v = len(depth[0]) / 2
 
     # Output the distance in meters from the center of the camera
-    rospy.loginfo("Dist: %s m", cv_image[u][v])
+    rospy.loginfo("Dist: %s m", depth[u][v])
+
+def map_callback(msg):
+    """Take in an image, convert it to a computer vision image, 
+    then get the value of the center pixel which should be 
+    the distance from the center of the camera."""
+    # type: (PointCloud2) -> None
+    
+    points = sensor_msgs.point_cloud2.read_points(msg)
+    point_list = []
+    for point in points:
+        point_list.append(point)
+
+    np_point_list = np.array(point_list)
+
+    # Get the center of the image matrix
+    u = len(np_point_list) / 2
+
+    # Output the distance in meters from the center of the camera
+    rospy.loginfo("Dist: %s m", np_point_list[u])
     
 def depth_subscriber():
     # Initialize the depth sensing node
     rospy.init_node('depth_sensing', anonymous=True)
 
     # Initialize the subscriber to take in an image message from the specified topic
-    rospy.Subscriber('/zedm/zed_node/depth/depth_registered', Image, depth_callback)
+    # rospy.Subscriber('/zedm/zed_node/depth/depth_registered', Image, depth_callback)
+    rospy.Subscriber('/zedm/zed_node/mapping/fused_cloud', PointCloud2, map_callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
